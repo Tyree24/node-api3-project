77d8d9fe9b694c68f7823802e8565e3bb3cc4196
Tyree Lebeau <tyreelebeau@Tyrees-iMac.local>
2020-03-19 21:00:11 -0500

    userRouter middleware structured, 1/7 endpoints scructured

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
+const Users = require('./userDb.js');
+const Posts = require('../posts/postDb.js');
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  Users.get(req.query).then(users => {
+    res.status(200).json(users);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({ message: "Error retrieving the posts" });
+  });
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -34,14 +43,42 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params;
+  Users.getById(id).then(user => {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(404).json({ error: "The specified user does not exist" })
+    }
+  }).catch(err => console.log(err));
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ error: "Name required" });
+  } else if (typeof name != "string") {
+    res.status(400).json({ error: "Name must be a string" })
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  const { id: user_id } = req.params;
+  const { text } = req.body;
+
+  if (!req.body) {
+    res.status(400).json({ error: "Post requires body" });
+  } else if (!text) {
+    res.status(400).json({ error: "Post requires text" });
+  } else {
+    req.body = { user_id, text };
+    next();
+  }
 }
 
 module.exports = router;
